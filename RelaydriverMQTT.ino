796b1d4af1fc1f248b9fbb3f6fb3ac922881ab3d
James Kennewell <ieatbedrock@gmail.com>
2021-03-26 12:35:29 +1030

    Complete Rewrite of the Code Base
    Merged some standards set in MQTT-Lightswitch-Controller-V2 Code Base.
    
    Moved the MCP23017 Libary over to use adafruit's Adafruit_MCP23017
    Moved all config setup over to a new file, Main file doesnt need to be updated to get the code working.

--- RelaydriverMQTT.ino
+++ RelaydriverMQTT.ino
@@ -1,254 +1,35 @@
 #include <Wire.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-
-
-/* NETWORK SETTINGS */
-#define ENABLE_MAC_ADDRESS_ROM      true
-#define MAC_I2C_ADDR          0x50
-static uint8_t device_mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-EthernetClient network_device;
-
-
-/* MQTT SETTINGS */
-IPAddress mqtt_broker(XXX,XXX,XXX,XXX); // replace XXX,XXX,XXX,XXX with your ip like 192,168,1,108
-PubSubClient mqtt(network_device);
-
-String mqtt_client = "relays_";
-const char mqtt_user[] = "username";
-const char mqtt_pass[] = "password";
-const char mqtt_sub[] = "house/switchboard/01/relay/+/command";
-unsigned long mqtt_timmer = 0;
-
-
-/* WATCH DOG SETTINGS */
-#define ENABLE_EXTERNAL_WATCHDOG    true   // set this to false if you dont have a watchdog
-#define WATCHDOG_PIN          8
-bool watchdog_status = false;
-unsigned long watchdog_timmer = 0;
-
-
-/* RELAY SETTINGS */
-const PROGMEM byte ic2_addresses[8] = {32,33,34,35,36,37,38,39};
-uint16_t shield_statuses[8] = {0,0,0,0,0,0,0,0};
-
+#include <Adafruit_MCP23017.h>
 
 
 /**
- * Initial setup - called once
- *
- * @return void
+ * Topic: relays/{{relay-number: 1-99}}
+ * Payload relay on: 1
+ * Payload relay off: 0
  */
-void setup ()
-{
-  // Enable watchdog
-  if (ENABLE_EXTERNAL_WATCHDOG == true)
-  {
-    pinMode(WATCHDOG_PIN, OUTPUT);
-    digitalWrite(WATCHDOG_PIN, LOW);
-  }
-
-  Wire.begin();
-
-  // Obtain MAC address from ROM
-  if (ENABLE_MAC_ADDRESS_ROM == true)
-  {
-    device_mac[0] = readRegister(0xFA);
-    device_mac[1] = readRegister(0xFB);
-    device_mac[2] = readRegister(0xFC);
-    device_mac[3] = readRegister(0xFD);
-    device_mac[4] = readRegister(0xFE);
-    device_mac[5] = readRegister(0xFF);
-  }
-
-  // Wait for network connection
-  while (Ethernet.begin(device_mac) == 0)
-  {
-    delay(1000);
-  }
-
-  // Initializes the pseudo-random number generator
-  randomSeed(micros());
-
-  // Configure MQTT
-  mqtt_client = String(mqtt_client + device_mac[5]);
-  mqtt.setServer(mqtt_broker, 1883);
-  mqtt.setCallback(mqttCallback);
-
-  // Initialise relay shields
-  for (int i = 0; i < 8; i++)
-  {
-    initialiseShield(pgm_read_word(ic2_addresses + i));
-  }
-}
 
 
 
-/**
- * Main loop program
- *
- * @return void
- */
-void loop ()
-{
-  // Keep network connection
-  Ethernet.maintain();
-
-  // Reconnect to MQTT server
-  if (!mqtt.connected())
-  {
-    if (!mqttReconnect())
-    {
-      // Exit early of loop
-      return;
-    }
-  }
-
-  // Check for incoming MQTT messages
-  mqtt.loop();
-
-  // Prevent arduino from resetting if everything is fine
-  if (ENABLE_EXTERNAL_WATCHDOG == true)
-  {
-    resetWatchDog();
-  }
-}
+/*--------------------------- CONFIGURATION ------------------------------*/
+// Configuration should be done in the included file:
+#include "config.h"
 
 
 
-/**
- * MQTT reconnect
- * @return bool True on successful connection
- * @return bool False on failure to connect
- */
-bool mqttReconnect ()
-{
-  // Only attempt every 1 seconds to prevent congesting the network
-  if (!getTimer(mqtt_timmer, 1000))
-  {
-    return false;
-  }
+/*--------------------------- SETUP -------------------------------------*/
+EthernetClient network_client;
 
-  // Attempt to connect with random client name
-  if (mqtt.connect(String(mqtt_client + String(random(0xffff), HEX)).c_str(), mqtt_user, mqtt_pass))
-  {
-    // Subscribe to topic
-    mqtt.subscribe(mqtt_sub);
+PubSubClient mqtt_client(network_client);
 
-    // Connection was successful
-    return true;
-  }
+Adafruit_MCP23017 mcps[6];
 
-  // Still unable to connect
-  return false;
-}
-
-
-/**
- * Processes messages from subscribed MQTT topics
- *
- * @param char* topic The incoming topic
- * @param byte* payload The incoming message
- * @param int length The length of payload
- * @return void
- */
-void mqttCallback (char* topic, byte* payload, unsigned int length)
-{
-  turnRelayOnOrOff(getRelayFromTopic(topic), ((char)payload[0] == '1'));
-}
-
-
-
-/**
- * Initialise 1C2 shield
- *
- * @param int shield_address Shield address value
- * @return void
- */
-void initialiseShield (int shield_address)
-{
-    // Set I/O bank A to outputs
-    Wire.beginTransmission(shield_address);
-    Wire.write(0x00); // IODIRA register
-    Wire.write(0x00); // Set all of bank A to outputs
-    Wire.endTransmission();
-}
-
-
-/**
- * Reads byte value from given register
- *
- * @param byte reg The register
- * @return byte The value
- */
-byte readRegister (byte reg)
-{
-  Wire.beginTransmission(MAC_I2C_ADDR);
-  // Register to read
-  Wire.write(reg);
-  Wire.endTransmission();
-
-  // Read a byte
-  Wire.requestFrom(MAC_I2C_ADDR, 1);
-  while(!Wire.available())
-  {
-    // Wait
-  }
-
-  return Wire.read();
-}
-
-
-/**
- * Sends given value to latch
- *
- * @param int shield_address Zero indexed shield address lookup
- * @param uint_16t rawValue The value to send
- * @return void
- */
-void sendRawValueToLatch (int shield_address, uint16_t raw_value)
-{
-  Wire.beginTransmission(shield_address);
-  Wire.write(0x12);
-  Wire.write(raw_value & 0xFF); // Assume the bank A has relays 1-8
-  Wire.endTransmission();
-}
-
-
-
-/**
- * Switch given relay ON or OFF
- *
- * @param byte relay_no The relay to switch
- * @param bool is_on True for ON, False for OFF
- * @return void
- */
-void turnRelayOnOrOff (byte relay_no, bool is_on)
-{
-  #ifdef RELAY_16
-    byte shield_number = ceil(((int) relay_no -1 ) / 16);
-    byte relay_number = (int) relay_no % 16;
-    if ((int) relay_number == 0) relay_number = 16;
-  #else
-    byte shield_number = ceil(((int) relay_no - 1) / 8);
-    byte relay_number = (int) relay_no % 8;
-    if ((int) relay_number == 0) relay_number = 8;
-  #endif
-
-  byte shield_i2c_address = pgm_read_word(ic2_addresses + shield_number);
-  uint16_t bit_mask = 1 << ((int) relay_number - 1);
-
-  if (is_on)
-  {
-    shield_statuses[shield_number] |= bit_mask;
-  }
-  else
-  {
-    shield_statuses[shield_number] &= ~bit_mask;
-  }
-
-  sendRawValueToLatch(shield_i2c_address, shield_statuses[shield_number]);
-}
+unsigned long watchdog_timer = 0;
+unsigned long mqtt_timer = 0;
+unsigned long heart_timer = 0;
+char mqtt_buffer[100];
+/*------------------------------------------------------------------------*/
 
 
 
@@ -256,11 +37,11 @@ void turnRelayOnOrOff (byte relay_no, bool is_on)
  * Determine if given timer has reached given interval
  *
  * @param unsigned long tmr The current timer
- * @param int interval Length of time to run timer
+ * @param unsigned long interval Length of time to run timer
  * @return bool True when timer is complete
  * @return bool False when timer is counting
  */
-bool getTimer (unsigned long &tmr, int interval)
+bool getTimer(unsigned long &tmr, unsigned long interval)
 {
   // Set initial value
   if (tmr < 1)
@@ -283,23 +64,85 @@ bool getTimer (unsigned long &tmr, int interval)
 
 
 /**
- * Fetch relay number from topic
+ * Converts IP address to char array
  *
- * @param String topic The given topic
- * @return int Relay number
+ * @param IPAddress address
+ * @return char*
  */
-int getRelayFromTopic(const String &topic)
+char *ip2CharArray(IPAddress address)
 {
-  int s, e = -1;
-  for (s = topic.length() -1; s > 0; --s)
+  static char ip[16];
+  sprintf(ip, "%d.%d.%d.%d", address[0], address[1], address[2], address[3]);
+  return ip;
+}
+
+
+
+/**
+ * Switch given relay ON or OFF
+ *
+ * @param byte relay_no The relay to switch
+ * @param bool is_on True for ON, False for OFF
+ * @return void
+ */
+void turnRelayOnOrOff(byte relay_no, bool is_on)
+{
+  #ifdef RELAY_16
+    byte shield_number = ceil(((int)relay_no - 1) / 16);
+    byte relay_number = (int)relay_no % 16;
+    if ((int)relay_number == 0) relay_number = 16;
+  #else
+    byte shield_number = ceil(((int)relay_no - 1) / 8);
+    byte relay_number = (int)relay_no % 8;
+    if ((int)relay_number == 0) relay_number = 8;
+  #endif
+
+  mcps[(uint16_t)shield_number].digitalWrite((uint16_t)relay_number-1, (uint16_t) is_on);
+}
+
+
+
+/**
+ * Reconnect to MQTT broker
+ * @return bool True on successful connection
+ * @return bool False on failure to connect
+ */
+bool mqttReconnect()
+{
+  // Too early to reconnect
+  if (!getTimer(mqtt_timer, 1000))
   {
-    if (isDigit(topic[s]))
-    {
-      if (e == -1) e = s;
-    }
-    else if (e != -1) break;
+    return false;
   }
-  return topic.substring(s + 1, e + 1).toInt();
+
+  // Connection successful
+  if (mqtt_client.connect(String(mqtt_name + String(random(0xffff), HEX)).c_str(), mqtt_user, mqtt_pass))
+  {
+    // Publish json-string payload of connection status: event type, local IP, controller ID
+    sprintf(mqtt_buffer, "{\"event\":\"reconnected\",\"ip\":\"%s\",\"device\":\"relay%d\"}", ip2CharArray(Ethernet.localIP()), controller_id);
+    mqtt_client.publish(mqtt_status_topic, mqtt_buffer);
+    mqtt_client.subscribe(mqtt_sub);
+
+    return true;
+  }
+
+  // Connection failed
+  return false;
+}
+
+
+
+/**
+ * Receive MQTT message
+ *
+ * @param char* topic The incomnig topic
+ * @param byte* payload The incoming message
+ * @param int leng Length of payload
+ * @return void
+ */
+void mqttReceive(char *topic, byte *payload, unsigned int leng)
+{
+  turnRelayOnOrOff(getRelayFromTopic(topic), ((char)payload[0] == '1'));
 }
 
 
@@ -310,12 +153,158 @@ int getRelayFromTopic(const String &topic)
  *
  * @return void
  */
+#ifdef ENABLE_WATCHDOG
 void resetWatchDog()
 {
-  if (getTimer(watchdog_timmer, 1000))
+  if (getTimer(watchdog_timer, 30000))
   {
-    watchdog_status = !watchdog_status;
+    digitalWrite(watchdog_pin, HIGH);
+    delay(20);
+    digitalWrite(watchdog_pin, LOW);
+  }
+}
+#endif
+
+
+
+/**
+ * Reads byte value from given register
+ *
+ * @param byte reg The register
+ * @return byte The value at given register
+ */
+byte readRegister(byte reg)
+{
+  Wire.beginTransmission(MAC_I2C_ADDR);
+
+  // Register to read
+  Wire.write(reg);
+  Wire.endTransmission();
+
+  // Read a byte
+  Wire.requestFrom(MAC_I2C_ADDR, 1);
+  while (!Wire.available())
+  {
+    // Wait
   }
 
-  digitalWrite(WATCHDOG_PIN, watchdog_status);
+  return Wire.read();
+}
+
+
+
+/**
+ * Fetch relay number from topic
+ *
+ * @param String topic The given topic
+ * @return int Relay number
+ */
+int getRelayFromTopic(const String &topic)
+{
+  int s, e = -1;
+  for (s = topic.length() - 1; s > 0; --s)
+  {
+    if (isDigit(topic[s]))
+    {
+      if (e == -1)
+        e = s;
+    }
+    else if (e != -1)
+      break;
+  }
+  return topic.substring(s + 1, e + 1).toInt();
+}
+
+
+
+/**
+ * Initial setup
+ *
+ * @return void
+ */
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+
+  #ifdef ENABLE_WATCHDOG
+    pinMode(watchdog_pin, OUTPUT);
+    digitalWrite(watchdog_pin, LOW);
+  #endif
+
+  #ifdef ENABLE_MAC_ADDRESS_ROM
+    ardunio_mac[0] = readRegister(0xFA);
+    ardunio_mac[1] = readRegister(0xFB);
+    ardunio_mac[2] = readRegister(0xFC);
+    ardunio_mac[3] = readRegister(0xFD);
+    ardunio_mac[4] = readRegister(0xFE);
+    ardunio_mac[5] = readRegister(0xFF);
+  #endif
+
+  #ifdef ENABLE_DHCP
+    // Wait for network connection
+    while (Ethernet.begin(ardunio_mac) == 0)
+    {
+      delay(1000);
+    }
+  #else
+    Ethernet.begin(ardunio_mac, ardunio_ip);
+  #endif
+
+  // Initializes the pseudo-random number generator
+  randomSeed(micros());
+
+  // Setup MQTT
+  mqtt_client.setServer(mqtt_broker, 1883);
+  mqtt_client.setCallback(mqttReceive);
+
+  // Setup pins
+  for (signed char i = 0; i < 6; i++)
+  {
+    mcps[i].begin(i);
+
+    for (signed char p = 0; p < 16; p ++)
+    {
+      mcps[i].pinMode(p, OUTPUT);
+    }
+  }
+}
+
+
+
+/**
+ * Main program loop
+ *
+ * @return void
+ */
+void loop()
+{
+  // Keep network connection
+  Ethernet.maintain();
+
+  // Reconnect to MQTT server
+  if (!mqtt_client.connected())
+  {
+    if (!mqttReconnect())
+    {
+      // Exit early of main loop
+      return;
+    }
+  }
+
+  // Check for incoming MQTT messages
+  mqtt_client.loop();
+
+  // Prevent arduino from resetting if everything is fine
+  #ifdef ENABLE_WATCHDOG
+    resetWatchDog();
+  #endif
+
+  // Heartbeat report
+  if (getTimer(heart_timer, HEARTBEAT))
+  {
+      // Publish json-string payload of connection status: event type, local IP, controller ID
+      sprintf(mqtt_buffer, "{\"event\":\"status\",\"ip\":\"%s\",\"device\":\"relay%d\"}", ip2CharArray(Ethernet.localIP()), controller_id);
+      mqtt_client.publish(mqtt_status_topic, mqtt_buffer);
+  }
 }
